3f20387e6ec2be0dc8fc8140a0b49408540433f7
dongminJang <qweuio456@naver.com>
2022-11-10 16:12:59 +0900

    프로그래머스 스택/큐 문제(주식가격)
    프로그래머스 스택/큐 문제(주식가격) 코드

---
+++ stack_queue6.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(prices):
+    queue = deque(prices) # 가격을 큐로 저장
+    answer = []
+
+    while queue:
+        price = queue.popleft() # 인덱스가 0인 가격
+        sec = 0
+        for q in queue: # 가격이 떨어질 때 까지 계산
+            sec = sec + 1
+            if price > q:
+                break
+        answer.append(sec) # 각각 나온 초를 저장
+
+    return answer
