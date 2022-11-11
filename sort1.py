26a194a1b054769bf0bc646aea71ca0e260db427
dongminJang <qweuio456@naver.com>
2022-11-11 10:49:55 +0900

    프로그래머스 정렬 문제(K번째수)
    프로그래머스 정렬 문제(K번째수) 코드

---
+++ sort1.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for i in commands:
+        arr = array[i[0] - 1:i[1]]
+        arr.sort()
+        answer.append(arr[i[2] - 1])
+    return answer
+
+# 출력
+print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
