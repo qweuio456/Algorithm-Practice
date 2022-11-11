ee29106c1eae2fa774e585ca0f1e092be5005872
dongminJang <qweuio456@naver.com>
2022-11-11 10:45:36 +0900

    프로그래머스 스택/큐 문제(주식가격)
    프로그래머스 스택/큐 문제(주식가격) 코드

--- stack_queue6.py
+++ stack_queue6.py
@@ -15,21 +15,6 @@ def solution(prices):
 
     return answer
 
-# 해답
-print(solution([1,2,3,2,3]))
-
-def solution(prices):
-    answer = [0] * len(prices)
-    for i in range(len(prices)):
-        for j in range(i + 1, len(prices)):
-            if prices[i] <= prices[j]:
-                answer[i] = answer[i] + 1
-            else:
-                answer[i] = answer[i] + 1
-                break
-
-    return answer
-
-# 해답
+# 출력
 print(solution([1,2,3,2,3]))
 
